0f5117711d9c8500ab76c335a55338ee35a4be7c
XTR Softwares 🕵️ <cwilly403@gmail.com>
2026-05-26 18:24:23 +0300

    Fix typo in __file__ reference

--- scan.py
+++ scan.py
@@ -1,6 +1,6 @@
 import sys as _sys
 import os as _os
-
+#_sys.path.insert(0, _os.path.dirname(_os.path.abspath(_file_)))
 _sys.path.insert(0, _os.path.dirname(_os.path.abspath(__file__)))
 
 import config as _cfg
